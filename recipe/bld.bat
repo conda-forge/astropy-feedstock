a0e921101192c66c80b47c84ec9253417175d5a7
xoviat <xoviat@users.noreply.github.com>
2018-02-06 14:08:10 -0600

    Use 32-bit compiler on 64 bit

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,7 @@
+if "%ARCH%"=="64" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+)
+
 %PYTHON% setup.py install --offline --old-and-unmanageable
 if errorlevel 1 exit 1
 
