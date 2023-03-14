47de2b264ae2c3565352963303248b8d26b6db40
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2023-03-14 21:38:21 +0100

    Revert run_test.py to version of 5.0.4 release

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,11 +1,11 @@
 import platform
 
 # import astropy._compiler
-import astropy.cosmology.flrw.scalar_inv_efuncs
+import astropy.cosmology.scalar_inv_efuncs
 import astropy.io.ascii.cparser
 import astropy.io.fits.compression
 import astropy.io.votable.tablewriter
-import astropy.modeling.projections
+import astropy.modeling._projections
 import astropy.timeseries.periodograms.lombscargle.implementations.cython_impl
 import astropy.table._column_mixins
 import astropy.table._np_utils
