3211554c3b580e69cffb713c14b69fd3b3b06d15
Stuart Mumford <stuart@cadair.com>
2024-11-26 16:52:38 +0000

    Fix tests

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,11 +1,9 @@
 import platform
 
 # import astropy._compiler
-import astropy.cosmology.flrw.scalar_inv_efuncs
 import astropy.io.ascii.cparser
 import astropy.io.votable.tablewriter
 import astropy.modeling.projections
-import astropy.timeseries.periodograms.lombscargle.implementations.cython_impl
 import astropy.table._column_mixins
 import astropy.table._np_utils
 import astropy.utils._compiler
